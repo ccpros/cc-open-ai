98dafc4dd964ecc90a23ff9b110c7ffc4c7130a6
Robin Cornett <robin.cornett@student.uagc.edu>
2025-06-23 17:49:55 -0700

    commit for cortex repair

---
+++ app/chiefcommand/components/Avatar.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useGLTF, useAnimations } from '@react-three/drei'
+import { useRef, useEffect } from 'react'
+import * as THREE from 'three'
+
+export default function Avatar(props: any) {
+  const group = useRef<THREE.Group | null>(null)
+  const { scene, animations } = useGLTF('../public/models/avatar.glb')
+  const { actions } = useAnimations(animations, group as React.MutableRefObject<THREE.Object3D>)
+
+  useEffect(() => {
+  const firstAnimation = Object.keys(actions)[0]
+  const action = actions[firstAnimation]
+  if (action) {
+    action.reset().fadeIn(0.5).play()
+  }
+}, [actions])
+
+
+  return <primitive ref={group} object={scene} {...props} />
+}
+
+useGLTF.preload('/models/avatar.glb')
--- app/chiefcommand/components/HologramScene.tsx
+++ app/chiefcommand/components/HologramScene.tsx
@@ -4,12 +4,12 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars, Float } from '@react-three/drei'
 import { useRef } from 'react'
 import * as THREE from 'three'
+import Avatar from './Avatar'
 
 function BackgroundElements() {
   const groupRef = useRef<THREE.Group>(null)
 
-  // Subtle hover animation
-  useFrame(({ clock, mouse }) => {
+  useFrame(({ mouse }) => {
     if (groupRef.current) {
       groupRef.current.rotation.y = mouse.x * 0.5
       groupRef.current.rotation.x = mouse.y * 0.3
@@ -18,22 +18,22 @@ function BackgroundElements() {
 
   return (
     <group ref={groupRef}>
-      {/* Glowing grid floor */}
+      {/* Glowing Grid Floor */}
       <mesh rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[30, 30, 64, 64]} />
         <meshBasicMaterial
           wireframe
-          color={'cyan'}
+          color="cyan"
           transparent
           opacity={0.2}
         />
       </mesh>
 
-      {/* Floating light orb */}
+      {/* Floating Light Orb */}
       <Float speed={1.5} floatIntensity={2} rotationIntensity={1}>
         <mesh position={[0, 2, -3]}>
           <sphereGeometry args={[0.3, 32, 32]} />
-          <meshStandardMaterial emissive={'cyan'} color={'white'} />
+          <meshStandardMaterial emissive="cyan" color="white" />
         </mesh>
       </Float>
     </group>
@@ -44,10 +44,10 @@ export default function HologramScene() {
   return (
     <Canvas camera={{ position: [0, 1.5, 4], fov: 50 }}>
       <ambientLight intensity={0.3} />
-      <pointLight position={[2, 3, 2]} intensity={1} color="cyan" />
+      <pointLight position={[2, 3, 2]} intensity={1.5} color="cyan" />
       <Stars radius={100} depth={50} count={5000} factor={4} fade />
       <BackgroundElements />
-      {/* Avatar will go here next */}
+      <Avatar position={[0, -1.5, 0]} />
       <OrbitControls enableZoom={false} />
     </Canvas>
   )
